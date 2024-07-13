6c3540e942645473d866a83084a16ad3b127cf72
Mike Saint-Antoine <msa406@nyu.edu>
2024-07-13 07:50:03 -0400

    add .py file

--- project1/transcriptomics.py
+++ project1/transcriptomics.py
@@ -1,6 +1,9 @@
 import numpy as np
 import csv
-
+import random
+import matplotlib.pyplot as plt
+from scipy.stats import ttest_ind, ks_2samp
+import seaborn as sns
 
 
 genes = []
@@ -14,25 +17,15 @@ with open("data/GSE150910_gene-level_count_file.csv") as csvfile:
         data.append(row[1:])
 
 
-# first row is sample labels
 samples = data[0]
 data = data[1:]
 genes = genes[1:]
 
 
-# everything is read in as strings, so we need to cast the data to floats
 data = np.array(data).astype(float)
-
-# also cast samples and genes to np array
 genes = np.array(genes)
 samples = np.array(samples)
 
-
-
-# shape checks
-print(genes.shape)
-print(samples.shape)
-print(data.shape)
 
 
 labels = []
@@ -41,25 +34,15 @@ for i in range(len(samples)):
     tmp = samples[i].split("_")
     labels.append(tmp[0])
     
-
 labels = np.array(labels)
 
 
-# set(labels)
+
 
 data = data[:,labels != "chp"]
 samples = samples[labels != "chp"]
 labels = labels[labels != "chp"]
 
-
-print(data.shape)
-print(samples.shape)
-print(labels.shape)
-
-
-
-
-# rough normalization
 
 
 for j in range(data.shape[1]):
@@ -69,54 +52,41 @@ for j in range(data.shape[1]):
 
 
 
-
-
-
-
-# differential expression analysis
-
-from scipy.stats import ks_2samp
-
+# t-test attempt, now for every gene
 
 p_values = []
 log2_FCs = []
 
+epsilon = 1e-8  # small constant to avoid division by zero
 
 for i in range(data.shape[0]):
     
-    
-    control = data[i,labels == "control"]
-    ipf = data[i,labels == "ipf"]
-    
-    # get p-value
-    ks_statistic, p_value = ks_2samp(control, ipf)
-    p_values.append(p_value)
-    
-    control_mean = np.mean(control)
-    if control_mean == 0:
-        control_mean = 0.00000001
-        # to avoid division by 0 error
+    control = data[i, labels == "control"]
+    ipf = data[i, labels == "ipf"]
 
-        
+    # perform t-test
+    t_statistic, p_value = ttest_ind(control, ipf, equal_var=False)
+    
+    ## if you'd rather do the KS test, just use this line instead
+    # ks_statistic, p_value = ks_2samp(control, ipf)
+
+    # save p_value for this gene
+    p_values.append(p_value)
+
+
+    # calculate means for each condition
+    control_mean = np.mean(control)
     ipf_mean = np.mean(ipf)
-    if ipf_mean == 0:
-        ipf_mean = 0.00000001
-        # to avoid division by 0 error
-        
-        
-    # get log2_FC
-    FC = ipf_mean / control_mean
+
+    # calculate log2(fold change), adding epsilon to avoid division by 0
+    FC = (ipf_mean + epsilon) / (control_mean + epsilon)
     log2_FC = np.log2(FC)
+    
+    # save log2_FC for this gene
     log2_FCs.append(log2_FC)
 
 
-
-
-
-
-
-print(len(p_values))
-
+# cast results to numpy arrays
 p_values = np.array(p_values)
 log2_FCs = np.array(log2_FCs)
 
@@ -124,42 +94,7 @@ log2_FCs = np.array(log2_FCs)
 p_values_bonf = p_values * len(genes)
 
 
-# print(log2_FCs)
-
-
-# a = np.array([True, True, False])
-# b = np.array([False, True, False])
-
-# print(a & b)
-
-
-sum((p_values_bonf <= 0.01) & (np.abs(log2_FCs) > 2))
-
-
-to_keep = (p_values_bonf <= 0.05) & (np.abs(log2_FCs) >= 2)
-
-
-
-# print(log2_FCs[to_keep])
-
-print(sum(to_keep))
-
-
-
-
-
-
-# saving significant DEGs
-
-
-print(len(to_keep))
-print(len(genes))
-print(len(p_values_bonf))
-print(len(log2_FCs))
-print(data.shape[0])
-
-
-
+to_keep = (p_values_bonf <= 0.01) & (np.abs(log2_FCs) >= 2)
 
 sig_genes = genes[to_keep]
 sig_log2_FCs = log2_FCs[to_keep]
@@ -167,49 +102,60 @@ sig_p_values_bonf = p_values_bonf[to_keep]
 sig_data = data[to_keep, :]
 
 
-print(len(sig_genes))
-
-print(sig_data.shape)
-
-
-
-
-# print out DEG lists
-
-for i in range(len(sig_genes)):
-    if sig_log2_FCs[i] > 0:
-        print(sig_genes[i])
+print(f"Number of DEGs: {len(sig_genes)}")
+print(f"Total number of genes: {len(genes)}")
+print(f"DEG percentage: {len(sig_genes) / len(genes) * 100}")
+print()
+print(f"Number of upregulated DEGs: {np.sum(sig_log2_FCs>0)}")
+print(f"Number of downregulated DEGs: {np.sum(sig_log2_FCs<0)}")
 
 
+# saving upregulated DEGs         
+output_file = "output/upregulated_DEGs.csv"
+with open(output_file, mode='w', newline='') as file:
+    writer = csv.writer(file)
+    
+    writer.writerow(["gene", "p_value", "log2_FC"])
+
+    for i in range(len(sig_genes)):
+        if sig_log2_FCs[i] > 0:
+            writer.writerow([sig_genes[i], sig_p_values_bonf[i], sig_log2_FCs[i]])
+
+
+# saving downregulated DEGs         
+output_file = "output/downregulated_DEGs.csv"
+with open(output_file, mode='w', newline='') as file:
+    writer = csv.writer(file)
+    
+    writer.writerow(["gene", "p_value", "log2_FC"])
+
+    for i in range(len(sig_genes)):
+        if sig_log2_FCs[i] < 0:
+            writer.writerow([sig_genes[i], sig_p_values_bonf[i], sig_log2_FCs[i]])
+
+
+sig_data_log2fc = np.zeros_like(sig_data)
 
 
 
-sig_data_log2fc = sig_data.copy()
+epsilon = 1e-8  # small constant to avoid division by zero
+
 
 for i in range(len(sig_genes)):
     
     control_mean =  np.mean(sig_data[i,labels == "control"])
     
-    sig_data_log2fc[i,:] = np.log2((sig_data_log2fc[i,:]+0.000001) / (control_mean +0.000001))
+    sig_data_log2fc[i,:] = np.log2((sig_data[i,:]+epsilon) / (control_mean +epsilon))
     
-    
-    
-import seaborn as sns
-import matplotlib.pyplot as plt
 
 
-sns_plot = sns.clustermap(sig_data_log2fc, xticklabels=samples, yticklabels= sig_genes, cmap="coolwarm", vmin=-2, vmax=2)
+sns_plot = sns.clustermap(sig_data_log2fc, xticklabels=samples, yticklabels= sig_genes, cmap="coolwarm", vmin=-6, vmax=6)
 
-# TODO - can this be done with easier syntax?
+# changing font sizes for x and y labels
 sns_plot.ax_heatmap.set_xticklabels(sns_plot.ax_heatmap.get_xmajorticklabels(), fontsize=2)
 sns_plot.ax_heatmap.set_yticklabels(sns_plot.ax_heatmap.get_ymajorticklabels(), fontsize=2)
 
 
-sns_plot.savefig("plots/heatmap.pdf")
+sns_plot.savefig("output/heatmap.pdf")
 
-# plt.show()
-
-
-
-
-print("testing")
+plt.show()
